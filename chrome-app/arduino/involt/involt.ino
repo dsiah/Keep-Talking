f9089138ebdfd822bf64cebef2908214603d1464
david siah <dsiah@villanova.edu>
2016-02-14 05:41:59 -0500

    portable

--- chrome-app/arduino/involt/involt.ino
+++ chrome-app/arduino/involt/involt.ino
@@ -6,6 +6,17 @@
   basic interactions. This mode is not recommended when
   mixing digital inputs and outputs.
 */
+
+// Potentiameter Constants
+const int lowA  = 10; 
+const int highA = 60;
+
+const int lowB  = 150;
+const int highB = 200;
+
+const int lowC  = 400;
+const int highC = 450;
+
 boolean autoPinMode = false;
 /*
   DIRECT MODE
@@ -45,13 +56,49 @@ void setup() {
 
 void loop() {
   //receive data from your app, do not remove this line.
-  involtReceive();
-  //  digitalWrite(5, involtDigital[5]);
+//  involtReceive();
 
-  involtSend(0, analogRead(A0));
+//  involtSendString(6, "See you, Space Cowboy");
+//  delay(2);
+
+    // input from agent
+  int lock0 = analogRead(A0);
+  int lock1 = analogRead(A1);
+  int lock2 = analogRead(A2);
+  int lock3 = analogRead(A3);
+  int lock4 = analogRead(A4);
+  int lock5 = analogRead(A5);
+  
+  // plot for expert
+  int safeA = lock1 - lock4;
+  int safeB = lock2 - lock3;
+  int safeC = lock0 - lock5;
+
+  // internal (not visible)
+  int openA = (lowA < safeA && safeA < highA) ? 1 : 0;
+  int openB = (lowB < safeB && safeB < highB) ? 1 : 0;
+  int openC = (lowC < safeC && safeC < highC) ? 1 : 0;
+  
+  // display as string/number to expert
+  int numOpen = openA + openB + openC; 
+
+//  Serial.print("safeA: ");
+//   Serial.print(safeA);
+//  Serial.print(", safeB: ");
+//   Serial.print(safeB);
+//  Serial.print(", safeC:");
+//   Serial.print(safeC);
+//  Serial.print("       ");
+//  Serial.print(", Safes Open:");
+//   Serial.println(numOpen);
+//  delay(2);
+  involtSendString(13, String(safeA));
   delay(2);
-  involtSend(1, analogRead(A1));
+  involtSendString(14, String(safeB));
   delay(2);
+  involtSendString(15, String(safeC));
+  delay(2);
+  involtSendString(16, String(numOpen));
 }
 
 //----------------------
@@ -116,3 +163,4 @@ void involtReceive() {
     
   };
 };
+
