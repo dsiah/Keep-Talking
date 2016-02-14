2e7bdbd96d8f046cd37c94c5fca7351a8eb1d593
david siah <dsiah@villanova.edu>
2016-02-14 04:23:16 -0500

    see you space cowboy

--- chrome-app/Safecracker.js
+++ chrome-app/Safecracker.js
@@ -4,10 +4,11 @@ var bar_one, bar_two, bar_three;
 function loop() {
 	console.log($('.A0').attr('value'));
 	console.log($('.A1').attr('value'));
-	console.log($('.A2').attr('value'));
-	console.log($('.A3').attr('value'));
-	console.log($('.A4').attr('value'));
-	console.log($('.A5').attr('value'));
+	// console.log($('.A2').attr('value'));
+	// console.log($('.A3').attr('value'));
+	// console.log($('.A4').attr('value'));
+	// console.log($('.A5').attr('value'));
+	console.log($('.A6').attr('value'));
 }
 
 setInterval(loop, 1000);
